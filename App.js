ac136466b4a966bab876f41303ac579957d2390f
Jonathan Bryant <spectrum19tech@gmail.com>
2023-05-24 10:43:57 -0700

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { TouchableOpacity, GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
+  const onSingleTap = () => {
+    alert('You tapped the screen!');
+    console.log("Hello there, JB!")
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000' }}>
+      <View>
+        <TouchableOpacity onPress={onSingleTap}>
+          <Text style={{ color: '#fff', fontSize: 20 }}>Tap Me!</Text>
+        </TouchableOpacity>
+      </View>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
